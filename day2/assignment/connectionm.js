22146a3f98d83cab50dade60fb3ef23f1b301705
Priti Gupta <priti.gupta@cuelogic.co.in>
2016-09-28 10:45:51 +0530

    new code

--- day2/assignment/connectionm.js
+++ day2/assignment/connectionm.js
@@ -1,50 +1,10 @@
-'use strict';
-
-const Hapi = require('hapi');
-
-const server = new Hapi.Server();
-server.connection({ port: 3000 });
-
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply("hello");
-    }
-});
-server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`);
-});
-
 var mongoose = require('mongoose');
 var db = mongoose.connection;
 db.on('error', console.error);
-mongoose.connect('mongodb://localhost/test_db');
 
-var userSchema = new mongoose.Schema({
-  id: { type: Number }
-, name: String
-, age: Number
+//module.exports.db = db;
+module.exports.mongoose = mongoose;
 
-});
-
-var user1 = mongoose.model('user', userSchema,'user');
-server.route({
-    method: 'GET',
-    path: '/userdata',
-    handler: function (request, reply) {
-    	user1.find(function(err, userr) {
-		  if (err) return console.error(err);
-		  
-		  reply(userr);
-		});
-        
-    }
-});
 
 
 
--- day2/assignment/fetchdata.js
+++ day2/assignment/fetchdata.js
@@ -1,15 +1,39 @@
 'use strict';
 var con = require('./connectionm');
 const Hapi = require('hapi');
+var mongoosec = con.mongoose;
 
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
+
 
 server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        reply(con.getUsers);
+        reply("hello");
+    }
+});
+mongoosec.connect('mongodb://localhost/test_db');
+
+var userSchema = new mongoosec.Schema({
+  id: { type: Number }
+, name: String
+, age: Number
+
+});
+
+var user1 = mongoosec.model('user', userSchema,'user');
+server.route({
+    method: 'GET',
+    path: '/userdata',
+    handler: function (request, reply) {
+    	user1.find(function(err, userr) {
+		  if (err) return console.error(err);
+		  
+		  reply(userr);
+		});
+        
     }
 });
 server.start((err) => {
