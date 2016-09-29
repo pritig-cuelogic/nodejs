a833ae43f91af7c8149e6d6c1359ff2f0941a6c5
Priti Gupta <priti.gupta@cuelogic.co.in>
2016-09-29 19:25:43 +0530

    friend api add assignment

--- day2/assignment/mysqlconnect/app.js
+++ day2/assignment/mysqlconnect/app.js
@@ -1,10 +1,10 @@
 'use strict';
-var connection      = require('./connection');
+var db      = require('./dboperation');
+var crypt   = require('crypto');
 
-
-var conn = connection.con;
 const Hapi = require('hapi');
 var Joi = require('joi');
+
 
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
@@ -21,14 +21,42 @@ server.route({
     method: 'GET',
     path: '/getdata',
     handler: function (request, reply) {
-    	conn.query('SELECT * FROM  user',function(err,rows){
-	  if(err) throw err;
-	  reply(rows);
-	  console.log('Data received from Db:\n');
-	  
-	});
-     
+      
+      db.fetchAllUsers().then(function(rows){
+          reply(rows);
+       });
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/login',
+    handler: function (request, reply) {
         
+        db.checkValiduser(request.payload.email,request.payload.password).then(function(rows){
+         
+         //reply(rows[0].id);
+         generateToken().then(function(token){
+            db.updateToken(1,token,rows[0].id).then(function(rows){
+                reply({token : token});
+            })
+            .catch(function(err){
+            reply(err);
+            });
+         });
+       })
+       .catch(function(err){
+            reply(err);
+       });
+       
+    },
+    config: {
+        validate: {
+            payload: {
+                email: Joi.string().email().required(),
+                password: Joi.string().alphanum().min(3).max(12).required()
+            }
+        }
     }
 });
 
@@ -37,25 +65,28 @@ server.route({
     path: '/insertdata',
     handler: function (request, reply) {
     	
-    	var userdata = { name: request.payload.name, email: request.payload.email, mobno: request.payload.mobno};
-
+    	var userdata = { name: request.payload.name, email: request.payload.email, mobno: request.payload.mobno,password:request.payload.password};
+        db.addUser(userdata).then(function(rows){
+           var luserdata = {user_id: rows.insertId};
+            db.addLoggedinUser(luserdata).then(function(rows){
+                reply("User Added Successfully.");
+            })
+                .catch(function(err){
+                    reply("Error :"+err);
+               });
+           })
+       .catch(function(err){
+            reply("Error :"+err);
+       });
        
-        
-    	conn.query('insert into user set ?',userdata,function(err,rows){
-	  if(err) throw err;
-	  reply("data inserted last id :"+rows.insertId);
-	  console.log('Last insert ID:', rows.insertId);
-	  
-	});
-    
-        
     },
     config: {
         validate: {
             payload: {
                 name: Joi.string().required(),
                 email: Joi.string().email().required(),
-                mobno: Joi.number().integer().required()
+                mobno: Joi.number().integer().required(),
+                password: Joi.string().alphanum().min(3).max(12).required()
             }
         }
     }
@@ -66,17 +97,23 @@ server.route({
     path: '/updatedata/{id}',
     handler: function (request, reply) {
     	
-    	var userdata = {name: request.payload.name,email: request.payload.email, mobno: request.payload.mobno};
-    	//console.log(userdata);
-    	//console.log(encodeURIComponent(request.params.id));
-    	conn.query('update user set name = ?,email = ?, mobno=? where id = ? ',[userdata.name,userdata.email,userdata.mobno,encodeURIComponent(request.params.id)],function(err,rows){
-	  if(err) throw err;
-	  reply(rows.changedRows +"data updated   :");
-	  console.log(rows.changedRows +'data updated with id  :');
-	  
-	});
-     
-        
+         db.checkloggedinUser(request.headers.authtoken).then(function(rows){
+            if(rows.length >=1){
+            
+                var userdata = {name: request.payload.name,email: request.payload.email, mobno: request.payload.mobno};
+                var id1 = encodeURIComponent(request.params.id);
+                db.updateUser(userdata,id1).then(function(rows){
+                  reply(rows.changedRows +"data updated   :");
+               });
+           
+        }
+            else{
+               reply("Please Logged in.");
+            }
+        })
+        .catch(function(err){
+            reply("Error :"+err);
+       });   	
     },
     config: {
         validate: {
@@ -96,20 +133,206 @@ server.route({
     method: 'DELETE',
     path: '/deletedata/{id}',
     handler: function (request, reply) {
-    	
-    	conn.query('delete from user where id = ? ',[request.params.id],function(err,rows){
-	  if(err) throw err;
-	  reply("deleted rows  :"+rows.affectedRows);
-	  console.log('deleted rows :', rows.affectedRows);
-	  
-	});
-     
-        
+    	db.checkloggedinUser(request.headers.authtoken).then(function(rows){
+            if(rows.length >=1){
+                var id1 = request.params.id;
+                db.deleteUser(id1).then(function(rows){
+                  reply("deleted rows  :"+rows.affectedRows);
+               });
+            }
+            else{
+               reply("Please Logged in."); 
+            }
+
+        })
+        .catch(function(err){
+            reply("Error :"+err);
+       }); 
     },
     config: {
         validate: {
             params: {
                 id: Joi.number().integer().required()
+            }
+        }
+    }
+});
+
+server.route({
+    method: 'PUT',
+    path: '/loggedout/{id}',
+    handler: function (request, reply) {
+        var id1 = request.params.id;
+        db.updateToken(0,'',id1).then(function(rows){
+                reply('User Logout successfully.');
+            })
+            .catch(function(err){
+            reply(err);
+            });
+    },
+    config: {
+        validate: {
+            params: {
+                id: Joi.number().integer().required()
+            }
+        }
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/addfriend',
+    handler: function (request, reply) {
+        db.checkloggedinUser(request.headers.authtoken).then(function(rows){
+
+         if(rows.length >=1){
+            var frienddata = {friend_id: request.payload.friend_id, user_id: rows[0].user_id };
+            db.addFriend(frienddata).then(function(rows){
+                reply("Friend added Successfully.");
+            })
+            .catch(function(err){
+            reply("Error :"+err);
+            });
+         }
+         else{
+            reply("Please Logged in.");
+         }
+         })
+        .catch(function(err){
+            reply("Error :"+err);
+       });   
+        
+
+    },
+    config: {
+        validate: {
+            payload: {
+                friend_id: Joi.number().integer().required()
+            }
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/getAllfriend',
+    handler: function (request, reply) {
+        db.checkloggedinUser(request.headers.authtoken).then(function(rows){
+                if(rows.length >=1){
+                    db.fetchAllFriend(rows[0].user_id).then(function(rows){
+                        reply(rows);
+                    })
+                    .catch(function(err){
+                        reply(err);
+                        });
+                }
+                else{
+                   reply("Please Logged in."); 
+                }
+        })
+        .catch(function(err){
+            reply(err);
+            });
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/viewfriend/{id}',
+    handler: function (request, reply) {
+        db.checkloggedinUser(request.headers.authtoken).then(function(rows){
+                if(rows.length >=1){
+                    var id1 = request.params.id;
+                    db.fetchOneFriend(rows[0].user_id,id1).then(function(rows){
+                        reply(rows);
+                    })
+                    .catch(function(err){
+                        reply(err);
+                        });
+                }
+                else{
+                   reply("Please Logged in."); 
+                }
+        })
+        .catch(function(err){
+            reply(err);
+            });
+    },
+    config: {
+        validate: {
+            params: {
+                id: Joi.number().integer().required()
+            }
+        }
+    }
+});
+
+server.route({
+    method: 'DELETE',
+    path: '/removefriend/{id}',
+    handler: function (request, reply) {
+        db.checkloggedinUser(request.headers.authtoken).then(function(rows){
+                if(rows.length >=1){
+                    var id1 = request.params.id;
+                    db.deleteFriend(rows[0].user_id,id1).then(function(rows){
+                        reply("Friend Deleted: "+rows.affectedRows);
+                    })
+                    .catch(function(err){
+                        reply(err);
+                        });
+                }
+                else{
+                   reply("Please Logged in."); 
+                }
+        })
+        .catch(function(err){
+            reply(err);
+            });
+    },
+    config: {
+        validate: {
+            params: {
+                id: Joi.number().integer().required()
+            }
+        }
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/searchfriend',
+    handler: function (request, reply) {
+        db.checkloggedinUser(request.headers.authtoken)
+            .then(function(rows){
+                if(rows.length >=1){
+                    return rows
+                }
+                else{
+                   reply("Please Logged in."); 
+                } 
+            })
+            .then(function(rows){
+                var name = request.payload.friend_name;
+                return db.searchFriendByName(rows[0].user_id,name);
+                    // .then(function(rows){
+                    //     reply(rows);
+                    // })
+                    // .catch(function(err){
+                    //     reply(err);
+                    // });
+            })
+            .then(function(result){
+                reply(result);
+            })
+            .catch(function(err){
+                reply(err);
+            });
+    },
+    config: {
+        validate: {
+            
+            payload: {
+                friend_name: Joi.string().required()
             }
         }
     }
@@ -122,3 +345,19 @@ server.start((err) => {
     }
     console.log(`Server running at: ${server.info.uri}`);
 });
+
+// generate random token
+
+var generateToken = function(){
+    return new Promise(function(resolve,reject){
+    crypt.randomBytes(20, function(err, buffer) {
+    var token = buffer.toString('hex');
+    if(err){             
+            return reject(err);
+        }else{ 
+                  
+            return resolve(token);
+        }
+    });
+    }) ;
+}
--- day2/assignment/mysqlconnect/dboperation.js
+++ day2/assignment/mysqlconnect/dboperation.js
@@ -1,19 +1,204 @@
 
+var Promise = require("promise");
 var connection      = require('./connection');
-const Hapi = require('hapi');
+
 var conn = connection.con;
 
-module.exports.fetchAllData = function(){
-	//var data;
-	var fun = function(err,rows){
-	  if(err) throw err;
-	  console.log(rows);
-	  console.log('Data received from Db:\n');
-	  return rows;
-	};
-	conn.query('SELECT * FROM  user',fun);
-	console.log('fun ==>', fun);
-	return fun;
+module.exports.fetchAllUsers = function(){
+	return new Promise(function(resolve,reject){
+    conn.query("select * from user",function(err,rows,fields){
+
+        if(err){                
+            return reject(err);
+        }else{              
+            return resolve(rows);
+        }
+
+    }); // query
+  }); 
+	
+};
+
+module.exports.addUser = function(userdata){
+	return new Promise(function(resolve,reject){
+    conn.query('insert into user set ?',userdata,function(err,rows,fields){
+
+        if(err){                
+            return reject(err);
+        }else{              
+            return resolve(rows);
+        }
+
+    }); // query
+  }); 
+	
+};
+
+module.exports.updateUser = function(userdata,id){
+	return new Promise(function(resolve,reject){
+    conn.query('update user set name = ?,email = ?, mobno=? where id = ? ',[userdata.name,userdata.email,userdata.mobno,id],function(err,rows){
+
+        if(err){                
+            return reject(err);
+        }else{              
+            return resolve(rows);
+        }
+
+    }); // query
+  }); 
+	
 };
 
 
+module.exports.deleteUser = function(id){
+	return new Promise(function(resolve,reject){
+    conn.query('delete from user where id = ? ',[id],function(err,rows){
+
+        if(err){                
+            return reject(err);
+        }else{              
+            return resolve(rows);
+        }
+
+    }); // query
+  }); 
+	
+};
+
+module.exports.checkValiduser = function(email,pass){
+	return new Promise(function(resolve,reject){
+    conn.query("select id from user where email = ? and password = ? ",[email,pass],function(err,rows,fields){
+
+        if(err){             
+            return reject(err);
+        }else{ 
+                  
+            return resolve(rows);
+        }
+
+    }); // query
+  }); 
+	
+};
+
+module.exports.updateToken = function(isloggedin,token,id){
+	return new Promise(function(resolve,reject){
+    conn.query('update login set isloggedin = ?, token = ? where user_id = ?',[isloggedin,token,id],function(err,rows){
+
+        if(err){                
+            return reject(err);
+        }else{              
+            return resolve(rows);
+        }
+
+    }); // query
+  }); 
+	
+};
+
+
+module.exports.addLoggedinUser = function(luserdata){
+	return new Promise(function(resolve,reject){
+    conn.query('insert into login set ?',luserdata,function(err,rows,fields){
+
+        if(err){                
+            return reject(err);
+        }else{              
+            return resolve(rows);
+        }
+
+    }); // query
+  }); 
+	
+};
+
+module.exports.checkloggedinUser = function(token){
+	return new Promise(function(resolve,reject){
+    conn.query("select user_id from login where token = ? ",[token],function(err,rows,fields){
+
+        if(err){             
+            return reject(err);
+        }else{ 
+                  
+            return resolve(rows);
+        }
+
+    }); // query
+  }); 
+	
+};
+
+module.exports.addFriend = function(frienddata){
+	return new Promise(function(resolve,reject){
+    conn.query('insert into friend set ?',frienddata,function(err,rows,fields){
+
+        if(err){                
+            return reject(err);
+        }else{              
+            return resolve(rows);
+        }
+
+    }); // query
+  }); 
+	
+};
+
+module.exports.fetchAllFriend = function(id){
+	return new Promise(function(resolve,reject){
+    conn.query('select u.name from user as u left join friend as f on u.id = f.friend_id where f.user_id = ? ',[id],function(err,rows,fields){
+    		
+        if(err){                
+            return reject(err);
+        }else{              
+            return resolve(rows);
+        }
+
+    }); // query
+  }); 
+	
+};
+
+module.exports.fetchOneFriend = function(user_id,id){
+	return new Promise(function(resolve,reject){
+    conn.query('select u.name, u.mobno from user as u left join friend as f on u.id = f.friend_id where f.user_id = ? and f.friend_id = ? ',[user_id,id],function(err,rows,fields){
+    		
+        if(err){                
+            return reject(err);
+        }else{              
+            return resolve(rows);
+        }
+
+    }); // query
+  }); 
+	
+};
+
+module.exports.deleteFriend = function(user_id,id){
+	return new Promise(function(resolve,reject){
+    conn.query('delete from friend where user_id = ? and friend_id = ? ',[user_id,id],function(err,rows,fields){
+    		
+        if(err){                
+            return reject(err);
+        }else{              
+            return resolve(rows);
+        }
+
+    }); // query
+  }); 
+	
+};
+
+module.exports.searchFriendByName = function(user_id,name){
+	return new Promise(function(resolve,reject){
+    conn.query('select name, mobno from user where id in(select friend_id from friend where user_id = ?) and name like "%'+name+'%"',[user_id],function(err,rows,fields){
+    		
+        if(err){ console.log(err);                
+            return reject(err);
+        }else{            
+            return resolve(rows);
+        }
+
+    }); // query
+  }); 
+	
+};
